9f1ab8a9cb8c99ff84946ed483e85f85467d5088
Tonny Gieselaar <tgiesela@gmail.com>
2024-09-07 16:52:08 +0200

    Changes for access via VPN and optimizations

---
+++ renewcertificates.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# To get the first certificate: 
+#	certbot certonly -d ${EXTERNALEMAILDOMAIN}
+source vars
+CERTFOLDER=/etc/letsencrypt/live/${EXTERNALEMAILDOMAIN}
+TARGETFOLDER=${DATAFOLDER}/certificates
+ORIGDATE=`(stat -L -c %Y $TARGETFOLDER/cert.pem)`
+certbot --standalone certonly -n -d ${EXTERNALEMAILDOMAIN}
+NEWDATE=`(stat -L -c %Y ${CERTFOLDER}/cert.pem)`
+
+echo "New date=" ${NEWDATE}",Original date="${ORIGDATE}
+
+if [ ${NEWDATE} == ${ORIGDATE} ]; then
+     echo "Certificate not renewed"
+else
+     echo "Certificate has changed"
+     echo "Installing the new certificate"
+     cp -p ${CERTFOLDER}/*.pem $TARGETFOLDER/
+     docker exec dovecot doveadm reload
+fi
